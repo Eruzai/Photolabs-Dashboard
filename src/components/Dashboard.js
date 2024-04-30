ef9e8873485e48cf4f3f680fa567c3ffdd767c3f
Kevin Levasseur <kevin_levasseur@telus.net>
2024-04-30 13:32:45 -0700

    feat: use api data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,35 +3,59 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
 class Dashboard extends Component {
-  state = { loading: false, focused: null };
+  state = {
+    loading: true,
+    focused: null,
+    photos: [],
+    topics: []
+  };
 
   componentDidMount() {
     const focused = JSON.parse(localStorage.getItem("focused"));
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics"
+    ].map(url => fetch(url).then(response => response.json()));
+
+    Promise.all(urlsPromise)
+    .then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics
+      });
+    });
 
     if (focused) {
       this.setState({ focused });
@@ -56,12 +80,12 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
-  const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
     .map((panel) => {
       return <Panel
         key={panel.id}
         label={panel.label}
-        value={panel.value}
+        value={panel.getValue(this.state)}
         onSelect={event => this.selectPanel(panel.id)}/>
     });
 
